a1123feb3dcec27ba65f266d997829fa960df648
garud-1 <142133577+garud-1@users.noreply.github.com>
2024-08-11 09:47:02 +0000

    added route for different pages

--- REACT/reactRouter/src/main.jsx
+++ REACT/reactRouter/src/main.jsx
@@ -1,32 +1,75 @@
-import { Children, StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+// import User from './components/User/User.jsx'
+// import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: '/',
-        element: <Home />
-      },
-      {
-        path: '/about',
-        element: <About />
-      },
-      {
-        path: '/contact',
-        element: <Contact />
-      }
-    ]
-  }
-]);
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router ={router}/>
-  </StrictMode>,
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout/>,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "about",
+//         element: <About />
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />
+//       }
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      {/* <Route path='user/:userid' element={<User />} /> */}
+      {/* <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element={<Github />}
+       /> */}
+    </Route>
+  )
 )
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
+
+
+/*
+In this we learn about how to use react Router in our project. We have created a simple project
+where we have created a layout component which is used to render the header and footer of the
+
+for using react router we hace to install react-router-donpm install react-router-dom
+then  for adding link and navlink instead of anchor tag 
+for this we have to import Link and NavLink from react router dom 
+then for routing we have to import Route and RouterProvider from react-router-dom
+then we have to create a router using createBrowserRouter which takes an array of objects
+each object has a path and element key
+but we have to methods to create a router
+1. createBrowserRouter
+2. createRoutesFromElements
+
+
+what if we create a route using createRoutesFromChildren 
+ 
+*/
