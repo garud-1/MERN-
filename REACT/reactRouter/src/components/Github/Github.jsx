9e6054e333f87f482de5911347b18be7e57fab69
garud-1 <142133577+garud-1@users.noreply.github.com>
2024-08-12 01:50:35 +0000

    added loader and async wait function instead if promise

--- REACT/reactRouter/src/components/Github/Github.jsx
+++ REACT/reactRouter/src/components/Github/Github.jsx
@@ -1,27 +1,32 @@
-import React, { useEffect,useState } from 'react'
- 
-// why we use this 
-// useParams is a hook that allows you to access the URL parameters of the current route.
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-  const [data, setData] = useState([])
-  useEffect(
-    ()=>{
-      fetch('https://api.github.com/users/garud-1')
-      .then(response=> response.json())
-      .then(data => {
-        console.log(data)
-        setData(data)
-      })
-       
-    }
-    ,[])
-   
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //  fetch('https://api.github.com/users/hiteshchoudhary')
+    //  .then(response => response.json())
+    //  .then(data => {
+    //     console.log(data);
+    //     setData(data)
+    //  })
+    // }, [])
+    
   return (
-    <div className='bg-gray-700 text-white text-3xl p-4 text-center m-4'>
-         Github Followers : {data.followers}
-         </div>
+    <>
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl rounded-sm'>Github followers: {data.followers}</div>
+    <div className='flex items-center justify-center'>
+    <img className='align-middle w-50 h-50 bg-blue-500 rounded-full'
+      src={data.avatar_url} alt="Git picture" width={300} />
+    </div>
+    </>
   )
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/garud-1')
+    return response.json()
+}
